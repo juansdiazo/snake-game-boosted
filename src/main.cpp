45926ce22b2667742f9f7aebaebca3b8b97df960
Juan Diaz <juansdiazo@gmail.com>
2020-06-05 19:32:11 +0200

    explanatory comments on base main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "renderer.h"
 
 int main() {
+  // Constant expressions that define misc. game settings:
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{640};
@@ -11,10 +12,15 @@ int main() {
   constexpr std::size_t kGridWidth{32};
   constexpr std::size_t kGridHeight{32};
 
+  // Main objects are created:
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
   Game game(kGridWidth, kGridHeight);
+
+  // Game loop is started
   game.Run(controller, renderer, kMsPerFrame);
+
+  // Printout at the end of the game
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
